ee677bc07ff9458b05a9e3851650a9bd50f17cab
ranggad990@gmail.com <ranggad990@gmail.com>
2018-08-10 09:58:41 +0700

    .

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '765613067636-4j1rm97bmeuk1imfb83bo3jtfq0bjbgl.apps.googleusercontent.com',
+	googleClientSecret: 'LU1owio1mpedP_1zg-dUBfBl',
+	mongoURI: 'mongodb://root:dbroot9@ds113732.mlab.com:13732/emaily-dev',
+	cookieKey: 'zdgvdsgfsgfdsgsdfhsertrsegrsegrse'
+};
